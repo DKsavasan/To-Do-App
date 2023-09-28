47ecc0c0c263b47f249b037123a167183b7bf850
DORUK SAVASAN <doruk@crc-dot1x-nat-10-239-127-122.bu.edu>
2023-09-28 17:54:05 -0400

    added crud operations

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv")
-dotenv.config()
 const mongoose = require("mongoose");
+
+const Todo = require("./models/Todo");
+
+dotenv.config()
 
 app.use("/static", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -10,14 +13,51 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get('/',(req, res) => {
-    res.render('todo.ejs');
+    Todo.find({}, (err, tasks) => {
+        res.render('todo.ejs', {todoTasks: tasks});
+    });
 });
 
-app.post('/',(req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const todoTask = new Todo({
+        content: req.body.content
+    });
+
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
 });
 
 mongoose.connect(process.env.DB_connect, {useNewUrlParser: true}, () => {
     console.log("Connected to the database");
     app.listen(3000, () => console.log("Server up and running"));
 });
+
+app
+.route("/edit/:id")
+.get((req, res) => {
+    const id = req.params.id;
+    Todo.find( {}, (err, tasks) => {
+        res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id});
+    });
+})
+
+.post((res, req) => {
+    const id = req.params.id;
+    Todo.findByIdAndUpdate(id, {content: req.body.content}, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    })
+})
+
+app
+.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    Todo.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    })
+})
